addc17701f674f4bc72af82f90f31030340d0c08
doxas <eizanco@gmail.com>
2014-12-04 23:29:41 +0900

    sphere model add

--- script/script.js
+++ script/script.js
@@ -23,11 +23,23 @@ function render(){
 	var vColor    = torusData.c;
 	var index     = torusData.i;
 	
-	var attVBO = [];
-	attVBO[0] = create_vbo(vPosition);
-	attVBO[1] = create_vbo(vNormal);
-	attVBO[2] = create_vbo(vColor);
-	var ibo = create_ibo(index);
+	var attTorusVBO = [];
+	attTorusVBO[0] = create_vbo(vPosition);
+	attTorusVBO[1] = create_vbo(vNormal);
+	attTorusVBO[2] = create_vbo(vColor);
+	var torusIbo = create_ibo(index);
+	
+	var sphereData = sphere(24, 24, 1.0);
+	vPosition = sphereData.p;
+	vNormal   = sphereData.n;
+	vColor    = sphereData.c;
+	index     = sphereData.i;
+	
+	var attSphereVBO = [];
+	attSphereVBO[0] = create_vbo(vPosition);
+	attSphereVBO[1] = create_vbo(vNormal);
+	attSphereVBO[2] = create_vbo(vColor);
+	var sphereIbo = create_ibo(index);
 	
 	var uniLocation = [];
 	uniLocation[0] = gl.getUniformLocation(prg, 'mMatrix');
@@ -68,18 +80,19 @@ function render(){
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		
 		gl.viewport(0, 0, cWidth, cHeight);
+		gl.uniform3fv(uniLocation[3], lightPosition);
+		gl.uniform3fv(uniLocation[4], eyePosition);
 		
 //		gl.activeTexture(gl.TEXTURE0);
 //		gl.bindTexture(gl.TEXTURE_2D, textures[0]);
 //		gl.activeTexture(gl.TEXTURE1);
 //		gl.bindTexture(gl.TEXTURE_2D, textures[1]);
 		
-		set_attribute(attVBO, attLocation, attStride);
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-		
 		m.lookAt(eyePosition, centerPoint, upDirection, vMatrix);
-		m.perspective(45, cWidth / cHeight, 0.1, 10.0, pMatrix);
+		m.perspective(45, cWidth / cHeight, 0.1, 100.0, pMatrix);
 		m.multiply(pMatrix, vMatrix, vpMatrix);
+		
+		set_attribute(attTorusVBO, attLocation, attStride, torusIbo);
 		
 		m.identity(mMatrix);
 		m.rotate(mMatrix, rad, [1.0, 1.0, 0.0], mMatrix);
@@ -90,10 +103,30 @@ function render(){
 		gl.uniformMatrix4fv(uniLocation[0], false, mMatrix);
 		gl.uniformMatrix4fv(uniLocation[1], false, mvpMatrix);
 		gl.uniformMatrix4fv(uniLocation[2], false, invMatrix);
-		gl.uniform3fv(uniLocation[3], lightPosition);
-		gl.uniform3fv(uniLocation[4], eyePosition);
 		
 		gl.drawElements(gl.TRIANGLES, torusData.i.length, gl.UNSIGNED_SHORT, 0);
+		
+		
+		
+		
+		
+		set_attribute(attSphereVBO, attLocation, attStride, sphereIbo);
+		
+		m.identity(mMatrix);
+//		m.translate(mMatrix, [50.0 - (count % 100), 0.0, 15.0], mMatrix);
+		m.rotate(mMatrix, Math.PI / 2, [0.0, 0.0, 1.0], mMatrix);
+		m.multiply(vpMatrix, mMatrix, mvpMatrix);
+		m.inverse(mMatrix, invMatrix);
+		
+		gl.uniformMatrix4fv(uniLocation[0], false, mMatrix);
+		gl.uniformMatrix4fv(uniLocation[1], false, mvpMatrix);
+		gl.uniformMatrix4fv(uniLocation[2], false, invMatrix);
+		
+		gl.drawElements(gl.TRIANGLES, sphereData.i.length, gl.UNSIGNED_SHORT, 0);
+		
+		
+		
+		
 		
 		gl.flush();
 		if(run){requestAnimationFrame(arguments.callee);}
@@ -132,171 +165,67 @@ window.onresize = function(){
 	}
 };
 
-// - 各種ユーティリティ関数 ---------------------------------------------------
-/**
- * シェーダを生成する関数
- * @param {string} source シェーダのソースとなるテキスト
- * @param {number} type シェーダのタイプを表す定数 gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
- * @return {object} 生成に成功した場合はシェーダオブジェクト、失敗した場合は null
- */
 function create_shader(source, type){
-	// シェーダを格納する変数
 	var shader;
-	
-	// シェーダの生成
 	shader = gl.createShader(type);
-	
-	// 生成されたシェーダにソースを割り当てる
 	gl.shaderSource(shader, source);
-	
-	// シェーダをコンパイルする
 	gl.compileShader(shader);
-	
-	// シェーダが正しくコンパイルされたかチェック
 	if(gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
-		
-		// 成功していたらシェーダを返して終了
 		return shader;
 	}else{
-		
-		// 失敗していたらエラーログをアラートする
 		alert(gl.getShaderInfoLog(shader));
-		
-		// null を返して終了
 		return null;
 	}
 }
 
-/**
- * プログラムオブジェクトを生成しシェーダをリンクする関数
- * @param {object} vs 頂点シェーダとして生成したシェーダオブジェクト
- * @param {object} fs フラグメントシェーダとして生成したシェーダオブジェクト
- * @return {object} 生成に成功した場合はプログラムオブジェクト、失敗した場合は null
- */
 function create_program(vs, fs){
-	// プログラムオブジェクトの生成
 	var program = gl.createProgram();
-	
-	// プログラムオブジェクトにシェーダを割り当てる
 	gl.attachShader(program, vs);
 	gl.attachShader(program, fs);
-	
-	// シェーダをリンク
 	gl.linkProgram(program);
-	
-	// シェーダのリンクが正しく行なわれたかチェック
 	if(gl.getProgramParameter(program, gl.LINK_STATUS)){
-	
-		// 成功していたらプログラムオブジェクトを有効にする
 		gl.useProgram(program);
-		
-		// プログラムオブジェクトを返して終了
 		return program;
 	}else{
-		
-		// 失敗していたらエラーログをアラートする
 		alert(gl.getProgramInfoLog(program));
-		
-		// null を返して終了
-		return null;
 	}
 }
 
-/**
- * VBOを生成する関数
- * @param {Array.<number>} data 頂点属性を格納した一次元配列
- * @return {object} 頂点バッファオブジェクト
- */
 function create_vbo(data){
-	// バッファオブジェクトの生成
 	var vbo = gl.createBuffer();
-	
-	// バッファをバインドする
 	gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-	
-	// バッファにデータをセット
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
-	
-	// バッファのバインドを無効化
 	gl.bindBuffer(gl.ARRAY_BUFFER, null);
-	
-	// 生成した VBO を返して終了
 	return vbo;
 }
 
-/**
- * IBOを生成する関数
- * @param {Array.<number>} data 頂点インデックスを格納した一次元配列
- * @return {object} インデックスバッファオブジェクト
- */
-function create_ibo(data){
-	// バッファオブジェクトの生成
-	var ibo = gl.createBuffer();
-	
-	// バッファをバインドする
+function set_attribute(vbo, attL, attS, ibo){
+	for(var i in vbo){
+		gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+		gl.enableVertexAttribArray(attL[i]);
+		gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+	}
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
-	
-	// バッファにデータをセット
+}
+
+function create_ibo(data){
+	var ibo = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
-	
-	// バッファのバインドを無効化
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-	
-	// 生成したIBOを返して終了
 	return ibo;
 }
 
-/**
- * VBOをバインドし登録する関数
- * @param {object} vbo 頂点バッファオブジェクト
- * @param {Array.<number>} attribute location を格納した配列
- * @param {Array.<number>} アトリビュートのストライドを格納した配列
- */
-function set_attribute(vbo, attL, attS){
-	// 引数として受け取った配列を処理する
-	for(var i in vbo){
-		// バッファをバインドする
-		gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
-		
-		// attributeLocationを有効にする
-		gl.enableVertexAttribArray(attL[i]);
-		
-		// attributeLocationを通知し登録する
-		gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
-	}
-}
-
-/**
- * テクスチャを生成する関数
- * @param {string} source テクスチャに適用する画像ファイルのパス
- * @param {number} number テクスチャ用配列に格納するためのインデックス
- */
 function create_texture(source, number){
-	// イメージオブジェクトの生成
 	var img = new Image();
-	
-	// データのオンロードをトリガーにする
 	img.onload = function(){
-		// テクスチャオブジェクトの生成
 		var tex = gl.createTexture();
-		
-		// テクスチャをバインドする
 		gl.bindTexture(gl.TEXTURE_2D, tex);
-		
-		// テクスチャへイメージを適用
 		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
-		
-		// ミップマップを生成
 		gl.generateMipmap(gl.TEXTURE_2D);
-		
-		// テクスチャのバインドを無効化
 		gl.bindTexture(gl.TEXTURE_2D, null);
-		
-		// 生成したテクスチャを変数に代入
 		textures[number] = tex;
 	};
-	
-	// イメージオブジェクトのソースを指定
 	img.src = source;
 }
 
